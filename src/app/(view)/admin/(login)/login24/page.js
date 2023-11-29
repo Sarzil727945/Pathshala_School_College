56d76f0944e59b5c1cbb39ff32d2ad78df53749d
fuck <atikshahriar986@gmail.com>
2023-11-29 14:58:51 +0600

    ok babu

--- src/app/(view)/admin/(login)/login24/page.js
+++ src/app/(view)/admin/(login)/login24/page.js
@@ -149,11 +149,11 @@ const Login24 = () => {
 
         var stats, update;
         stats = new Stats();
-        stats.setMode(0);
-        stats.domElement.style.position = 'absolute';
-        stats.domElement.style.left = '0px';
-        stats.domElement.style.top = '0px';
-        document.body.appendChild(stats.domElement);
+        // stats.setMode(0);
+        // stats.domElement.style.position = 'absolute';
+        // stats.domElement.style.left = '0px';
+        // stats.domElement.style.top = '0px';
+        // document.body.appendChild(stats.domElement);
 
         update = function () {
             stats.begin();
